849369cb7e0366964ec32212bad00ffa046db061
torlax <torlax@gmail.com>
2015-06-01 19:49:57 -0300

    Faça um Programa que peça dois números e imprima o maior deles.

---
+++ ex1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+num1 = input("Digite o primeiro numero. ")
+num2 = input("Digite o segundo numero. ")
+
+if num1 > num2:
+    print("O maior numero e: ",num1)
+elif num1 < num2:
+    print("O maior numero e: ",num2)
+else:
+    print("Numeros iguais!")
