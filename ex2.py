b8822df45b3266675beff55afe8781f2d7e386df
torlax <torlax@gmail.com>
2015-06-01 20:08:19 -0300

    Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.

---
+++ ex2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+num1 = input("Digite um numero. ")
+
+if num1 > 0:
+    print("O numero e positivo")
+elif num1 <0:
+    print("O numero e negativo")
+else:
+    print(" numero e Zero.") 
