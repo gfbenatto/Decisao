8c37e26fa8adaec6cdde9ab25d6c54956cdf6dfe
torlax <torlax@gmail.com>
2015-06-01 21:32:56 -0300

    Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve aparecer valor inválido.

---
+++ ex13.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+dia = input("Digite valores de 1 a 7 para cada dia da semana (ex: 1 - Domingo, 2 - Segunda...") 
+
+if dia == 1:
+    print("Domingo")
+
+elif dia == 2:
+    print("Segunda")
+
+elif dia == 3:
+    print("Terca")
+
+elif dia ==4:
+    print("Quarta")
+
+elif dia == 5:
+    print("Quinta")
+
+elif dia == 5:
+    print("Sexta")
+
+elif dia == 6:
+    print("Sabado")
+
+else:
+    print("Valor invalido.")
